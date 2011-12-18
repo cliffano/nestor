307700d52ad6485c1a526704bf171f17dd1cce5a
Cliffano Subagio <cliffano@gmail.com>
2011-12-18 20:47:42 +1100

    Add comm tests.

--- test/comm.js
+++ test/comm.js
@@ -1,15 +1,88 @@
 var assert = require('assert'),
   sandbox = require('sandboxed-module'),
+  url = require('url'),
   vows = require('vows');
 
 vows.describe('comm').addBatch({
+  'http': {
+    topic: function () {
+      return function (mocks) {
+        return sandbox.require('../lib/comm', mocks);
+      };
+    },
+    'when there is an error': {
+      topic: function (topic) {
+        topic({
+          requires: {
+            http: {
+              request: function (opts, cb) {
+                return {
+                  on: function (event, cb) {
+                    if (event === 'error') {
+                      cb(new Error('some error'));
+                    }
+                  },
+                  end: function () {
+                  }
+                };
+              }
+            }
+          }
+        }).http('/api/json', 'GET', url.parse('http://user:pass@localhost:8080'), this.callback);
+      },
+      'then the error should be passed via callback': function (err, result) {
+        assert.equal(err.message, 'some error');
+        assert.isUndefined(result);
+      }
+    },
+    'when there is no error': {
+      topic: function (topic) {
+        topic({
+          requires: {
+            http: {
+              request: function (opts, cb) {
+                // assert opts
+                return {
+                  on: function (event, cb) {
+                  },
+                  end: function () {
+                    cb({
+                      statusCode: 200,
+                      headers: ['some header'],
+                      setEncoding: function (encoding) {
+                        assert.equal(encoding, 'utf8');
+                      },
+                      on: function (event, cb) {
+                        if (event === 'data') {
+                          cb('some data');
+                        } else if (event === 'end') {
+                          cb();
+                        }
+                      }
+                    });
+                  }
+                };
+              }
+            }
+          }
+        }).http('/api/json', 'GET', url.parse('http://user:pass@localhost:8080'), this.callback);
+      },
+      'then the result should be passed via callback': function (err, result) {
+        assert.equal(result.statusCode, 200);
+        assert.equal(result.headers.length, 1);
+        assert.equal(result.headers[0], 'some header');
+        assert.equal(result.data, 'some data');
+        assert.isNull(err);
+      }
+    }
+  },
   'udp': {
     topic: function () {
       return function (mocks) {
         return sandbox.require('../lib/comm', mocks);
       };
     },
-    'when there is a socket error': {
+    'when there is an error event': {
       topic: function (topic) {
         topic({
           requires: {
@@ -19,11 +92,12 @@ vows.describe('comm').addBatch({
                 return {
                   on: function (event, cb) {
                     if (event === 'error') {
-                      assert.equal(event, 'message');
                       cb(new Error('some error'));
                     }
                   },
                   close: function () {
+                  },
+                  send: function (buffer, start, end, port, host, cb) {
                   }
                 };
               }
@@ -31,8 +105,74 @@ vows.describe('comm').addBatch({
           }
         }).udp('Long Live Jenkins', 'localhost', 8080, this.callback);
       },
-      'then an error should be passed via callback': function (error, result) {
+      'then the error should be passed via callback': function (err, result) {
         assert.equal(err.message, 'some error');
+        assert.isUndefined(result);
+      }
+    },
+    'when there is an error on socket send': {
+      topic: function (topic) {
+        topic({
+          requires: {
+            dgram: {
+              createSocket: function (type) {
+                assert.equal(type, 'udp4');
+                return {
+                  on: function (event, cb) {
+                  },
+                  close: function () {
+                  },
+                  send: function (buffer, start, end, port, host, cb) {
+                    cb(new Error('some error'));
+                  }
+                };
+              }
+            }
+          }
+        }).udp('Long Live Jenkins', 'localhost', 8080, this.callback);
+      },
+      'then the error should be passed via callback': function (err, result) {
+        assert.equal(err.message, 'some error');
+        assert.isUndefined(result);
+      }
+    },
+    'when there is no error': {
+      topic: function (topic) {
+        topic({
+          requires: {
+            xml2js: {
+              Parser: function () {
+                return {
+                  addListener: function (event, cb) {
+                    cb('some data');
+                  },
+                  parseString: function (data) {
+                  }
+                };
+              }
+            },
+            dgram: {
+              createSocket: function (type) {
+                assert.equal(type, 'udp4');
+                return {
+                  on: function (event, cb) {
+                    if (event === 'message') {
+                      cb('some data');
+                    }
+                  },
+                  close: function () {
+                  },
+                  send: function (buffer, start, end, port, host, cb) {
+                  }
+                };
+              }
+            }
+          }
+        }).udp('Long Live Jenkins', 'localhost', 8080, this.callback);
+      },
+      'then the data should be passed via callback': function (err, result) {
+        assert.equal(result, 'some data');
+        assert.isNull(err);
       }
     }
   }
