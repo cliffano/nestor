45f8da48f6834af2b6f7e77f1b17da6ffa72c702
Cliffano Subagio <cliffano@gmail.com>
2011-12-18 18:21:32 +1100

    Initial placeholder tests.

---
+++ test/cli.js
@@ -0,0 +1,13 @@
+var assert = require('assert'),
+  sandbox = require('sandboxed-module'),
+  vows = require('vows');
+
+vows.describe('cli').addBatch({
+  '': {
+    topic: function () {
+      return {};
+    },
+    '': function (topic) {
+    }
+  }
+}).exportTo(module);
