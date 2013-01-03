8302b831d14b0b58497d80b09b0f32f4734e2383
Cliffano Subagio <cliffano@gmail.com>
2013-01-03 16:42:49 +1100

    Add coverage reporting using buster-istanbul.

--- buster.js
+++ buster.js
@@ -1,6 +1,15 @@
 module.exports.unit = {
   environment: 'node',
+  sources: [
+    'lib/**/*.js'
+  ],
   tests: [
     'test/**/*.js'
-  ]
+  ],
+  extensions: [
+    require('buster-istanbul')
+  ],
+  'buster-istanbul': {
+    outputDirectory: '.bob/report/coverage'
+  }
 };
