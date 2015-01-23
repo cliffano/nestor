fc107ce012399f0c85a0730b0a806f1cedebc203
Cliffano Subagio <cliffano@gmail.com>
2015-01-23 18:24:19 +1100

    Add support for getting display color based on status and Jenkins color.

--- test/cli/util.js
+++ test/cli/util.js
@@ -6,6 +6,26 @@ var assert  = referee.assert;
 
 text.setLocale('en');
 
+buster.testCase('cli - util - colorByStatus', {
+  'should show the correct color when Jenkins color contains non-status and not animated color': function () {
+    assert.equals(util.colorByStatus('somestatus', 'blue'), 'blue');
+    assert.equals(util.colorByStatus('somestatus', 'green'), 'green');
+    assert.equals(util.colorByStatus('somestatus', 'grey'), 'grey');
+    assert.equals(util.colorByStatus('somestatus', 'red'), 'red');
+    assert.equals(util.colorByStatus('somestatus', 'yellow'), 'yellow');
+  },
+  'should show the correct color when Jenkins color contains animated color': function () {
+    assert.equals(util.colorByStatus('somestatus', 'blue_anime'), 'blue');
+    assert.equals(util.colorByStatus('somestatus', 'green_anime'), 'green');
+    assert.equals(util.colorByStatus('somestatus', 'grey_anime'), 'grey');
+    assert.equals(util.colorByStatus('somestatus', 'red_anime'), 'red');
+    assert.equals(util.colorByStatus('somestatus', 'yellow_anime'), 'yellow');
+  },
+  'should show grey color when Jenkins color is a status': function () {
+    assert.equals(util.colorByStatus('somestatus', 'somestatus'), 'grey');
+  }
+});
+
 buster.testCase('cli - util - statusByColor', {
   'should show the correct status for all supported colors': function () {
     assert.equals(util.statusByColor('blue'), 'ok');
